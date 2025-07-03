67975b6dadfe1fba0e19fbf54b462f718bbd9f5d
durdana_newuu <d.jenisbaeva@newuu.uz>
2025-07-03 09:43:33 +0500

    ML_concept

---
+++ matcha_day_1(cross-validation).py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+import numpy as np
+X,y = load_iris(return_X_y= True)
+model = DecisionTreeClassifier()
+scores = cross_val_score(model, X, y, cv = 5)
+print(f"Scores: {scores}")
+print(f"Average: {np.mean(scores)}")
---
+++ matcha_day_2(confusion_matrix).py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_iris
+data = load_iris()
+X = data.data
+Y = data.target
+
+print(X.shape)
+print(Y[:10])
